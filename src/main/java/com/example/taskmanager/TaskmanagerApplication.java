32baa5cde64a9629f76504697dbdb9e4d623c538
Amruta Wavdhane <amrutawavdhane@gmail.com>
2026-06-19 08:22:02 +0530

    .

--- src/main/java/com/example/taskmanager/TaskmanagerApplication.java
+++ src/main/java/com/example/taskmanager/TaskmanagerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TaskmanagerApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(TaskmanagerApplication.class, args);
+
+		 SpringApplication.run(TaskmanagerApplication.class, args);
 	}
 }
